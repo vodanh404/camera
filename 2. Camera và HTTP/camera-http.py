afbe2a8d5be2a8386f0cbfa5ca270689addcfae6
Đinh Viết Phúc <159451818+vodanh404@users.noreply.github.com>
2025-07-06 10:21:20 +0700

    Create camera-http.py

---
+++ 2. Camera và HTTP/camera-http.py
@@ -0,0 +1,27 @@
+# Cài thư viện bằng lệnh: pip install opencv-python flask
+# Khai báo thư viện
+from flask import Flask, Response
+import cv2
+import time
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0) # 0 là ID của webcam mặc định. Thay đổi nếu có nhiều camera.
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        ret, buffer = cv2.imencode('.jpg', frame) # Mã hóa khung hình thành JPEG
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.03) # Giảm tải CPU, khoảng 30 FPS
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+# Cách xem: truy cập vào  http://[Địa chỉ IP của máy phát]:5000/video_feed
